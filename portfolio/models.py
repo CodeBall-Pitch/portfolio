353223a759f69637d0dbb0e6e34a6053ad1541f0
Kiash254 <codewithkiash@gmail.com>
2023-01-02 22:41:42 +0300

    added models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Portfolio(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+    
+    
+class Education(models.Model):
+    institution = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+class Skills(models.Model):
+    skill = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
