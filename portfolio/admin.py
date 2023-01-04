ede5e495b67e01cf0183f18034c31e9b8c84e387
Kiash254 <codewithkiash@gmail.com>
2023-01-04 22:55:21 +0300

    created super user

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -7,18 +7,11 @@ admin.site.site_header = "Portfolio Admin"
 admin.site.site_title = "Portfolio Admin Area"
 admin.site.index_title = "Welcome to the Portfolio Admin Area"
 
-class PortfolioModelAdmin(admin.ModelAdmin):
-    list_display = ["title","image","description","url"]
-    list_display_links = ["title"]
-    list_filter = ["title"]
-    search_fields = ["title","description"]
-    class Meta:
-        model = Portfolio
         
-admin.site.register(Portfolio,PortfolioModelAdmin)
-admin.site.register(About,PortfolioModelAdmin)
-admin.site.register(Services,PortfolioModelAdmin)
-admin.site.register(Skills,PortfolioModelAdmin)
-admin.site.register(Contact,PortfolioModelAdmin)
-admin.site.register(Education,PortfolioModelAdmin)
-    
+admin.site.register(Portfolio)
+admin.site.register(About)
+admin.site.register(Services)
+admin.site.register(Skills)
+admin.site.register(Contact)
+admin.site.register(Education)
+
