f3e0c4cfe71782323fd41fe1ba8e37c5c46a9918
Kiash254 <codewithkiash@gmail.com>
2023-01-04 21:28:36 +0300

    aaded database models

--- portfolio/models.py
+++ portfolio/models.py
@@ -25,3 +25,28 @@ class Skills(models.Model):
 
     def __str__(self):
         return self.title
+    
+class About(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+class Services(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+class Contact(models.Model):
+        phone = models.CharField(max_length=200)
+        email = models.CharField(max_length=200)
+        subject = models.CharField(max_length=200)
+        description = models.CharField(max_length=200)
+        
+        def __str__(self):
+            return self.title
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from  .models import Portfolio, Education, Skills, About, Services, Contact
 # Create your views here.
 def Home(request):
-    return render(request, 'index.html')
+    portfolios = Portfolio.objects.all()
+    educations = Education.objects.all()
+    skills = Skills.objects.all()
+    abouts = About.objects.all()
+    services = Services.objects.all()
+    contacts = Contact.objects.all()
+        
+    data={
+        'portfolios': portfolios,
+        'educations': educations,
+        'skills': skills,
+        'abouts': abouts,
+        'services': services,
+        'contacts': contacts,
+    }
+    return render(request, 'index.html',data)
