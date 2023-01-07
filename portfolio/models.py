52768d8ad753cec417f39080c5832fb809fff313
Kiash254 <codewithkiash@gmail.com>
2023-01-07 22:47:56 +0300

    removerd the models duplicate

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 # Create your models here.
 class Portfolio(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+    image_thumbnail = ImageSpecField(source='image',
+                                        processors=[ResizeToFill(100, 50)],
+                                        format='JPEG',
+                                        options={'quality': 60})
+
 
     def __str__(self):
         return self.name
@@ -13,6 +19,11 @@ class Services(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+    image_service = ImageSpecField(source='image',
+                                        processors=[ResizeToFill(100, 50)],
+                                        format='JPEG',
+                                        options={'quality': 60})
+
 
     def __str__(self):
         return self.title
@@ -21,10 +32,16 @@ class Education(models.Model):
     institution = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+    image_education = ImageSpecField(source='image',
+                                        processors=[ResizeToFill(100, 50)],
+                                        format='JPEG',
+                                        options={'quality': 60})
+
 
     def __str__(self):
         return self.institution
 class Skills(models.Model):
+    course_name=models.CharField(max_length=200)
     percentage=models.PositiveIntegerField(default=0)
 
     def __str__(self):
@@ -34,6 +51,11 @@ class About(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+    image_about = ImageSpecField(source='image',
+                                        processors=[ResizeToFill(100, 50)],
+                                        format='JPEG',
+                                        options={'quality': 60})
+
 
     def __str__(self):
         return self.name
@@ -54,6 +76,11 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+    image_blog = ImageSpecField(source='image',
+                                        processors=[ResizeToFill(100, 50)],
+                                        format='JPEG',
+                                        options={'quality': 60})
+        
 
     def __str__(self):
         return self.title
