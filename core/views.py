10d560fdb38f5cd98af337efaedf4fd0f144f581
Kiash254 <codewithkiash@gmail.com>
2023-01-09 20:25:01 +0300

    added data on core

--- core/views.py
+++ core/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-
+from portfolio.models import Portfolio,About,Services,Skills,Contact,Education,Services
 # Create your views here.
 
 
 def IndexView(request):
-    
-    context={}
+    portfolios = Portfolio.objects.all()
+    educations = Education.objects.all()
+    skills = Skills.objects.all()
+    abouts = About.objects.all()
+    services = Services.objects.all()
+    contacts = Contact.objects.all()
+    context={
+        
+     
+        'portfolios': portfolios,
+        'educations': educations,
+        'skills': skills,
+        'abouts': abouts,
+        'services': services,
+        'contacts': contacts,  
+    }
     return render(request, 'index.html',context )
