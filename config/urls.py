3c7b98c6a414a7820faea4542180c1fee9540475
Kiash254 <codewithkiash@gmail.com>
2023-01-09 20:17:16 +0300

    added core on installed apps and also added urls

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("portfolio.urls",namespace='portfolio')),
+    path("core/", include("core.urls",namespace='core')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
