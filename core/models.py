3ca92270da7643b74ab71dff6a06fba2a80144fd
Kiash254 <codewithkiash@gmail.com>
2023-11-20 15:26:53 +0300

    changesed the database to mysql

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    aboutme=models.TextField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='imgaes')
+    
+    def __str__ (self):
+        return self.aboutme
+    
+    
+class Services(models.Model):
+    title=models.CharField()
+    desription=models.TextField()
+    
+    def __str__ (self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-from portfolio.models import Portfolio,About,Services,Skills,Contact,Education,Services
 # Create your views here.
-
+from .models import About
 
 def IndexView(request):
-    portfolios = Portfolio.objects.all()
-    educations = Education.objects.all()
-    skills = Skills.objects.all()
-    abouts = About.objects.all()
-    services = Services.objects.all()
-    contacts = Contact.objects.all()
-    context={
-        
-     
-        'portfolios': portfolios,
-        'educations': educations,
-        'skills': skills,
-        'abouts': abouts,
-        'services': services,
-        'contacts': contacts,  
+    
+    about=About.objects.all()
+   
+    context={ 
+        'about':about
     }
     return render(request, 'index.html',context )
