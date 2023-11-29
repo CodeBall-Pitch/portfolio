20f94d47e63f60c8a6ad6c5f46242c78ccbfc76b
Kiash254 <codewithkiash@gmail.com>
2023-11-29 18:04:45 +0300

    added new name on the title bar

--- portfolio/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from  .models import Portfolio, Education, Skills, About, Services, Contact
-# Create your views here.
-def Home(request):
-   
-    return render(request, 'index.html')
